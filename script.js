56caa533dfff20f8847557cc39f2e322759d5df6
cristian-albu <albucristian95p@gmail.com>
2023-10-14 12:23:45 +0300

    init

---
+++ script.js
@@ -0,0 +1,43 @@
+const draggables = document.querySelectorAll(".draggable");
+const containers = document.querySelectorAll(".container");
+
+draggables.forEach((e) => {
+    e.addEventListener("dragstart", () => {
+        e.classList.add("dragging");
+    });
+
+    e.addEventListener("dragend", () => {
+        e.classList.remove("dragging");
+    });
+});
+
+containers.forEach((container) => {
+    container.addEventListener("dragover", (e) => {
+        e.preventDefault();
+        const afterElement = getDragAfterElement(container, e.clientY);
+
+        const draggable = document.querySelector(".dragging");
+        if (afterElement == null) {
+            container.appendChild(draggable);
+        } else {
+            container.insertBefore(draggable, afterElement);
+        }
+    });
+});
+
+function getDragAfterElement(container, y) {
+    const draggableElements = [...container.querySelectorAll(".draggable:not(.dragging)")];
+
+    return draggableElements.reduce(
+        (closest, child) => {
+            const box = child.getBoundingClientRect();
+            const offset = y - box.top - box.height / 2;
+            if (offset < 0 && offset > closest.offset) {
+                return { offset: offset, element: child };
+            } else {
+                return closest;
+            }
+        },
+        { offset: Number.NEGATIVE_INFINITY }
+    ).element;
+}
